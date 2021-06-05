06bb9e858c0aa1e62234246d0381dd7db6d5e790
Vcheran <85411040+Vcheran@users.noreply.github.com>
2021-06-05 23:40:34 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Javascript and React.js</h1>
+      <p>
+      "Boot Camp is great.“What I learned will be useful to me throughout my career.”“My overall impression of the course is extremely positive.
+      </p>
+    </div>
+  );
+}
+
+export default Note;
